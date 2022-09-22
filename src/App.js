823775637f24e7841a5e698be4776d9db03192c7
vinay <vinaynn123@gmail.com>
2022-09-22 14:46:53 +0530

    added nav

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import { lazy, Suspense } from 'react';
 
 import Loading from './components/Loading';
+import Header from './components/Header';
 const Countrylist = lazy(() => import("./components/Countrylist"));
 
 function App() {
   return (
     <Suspense fallback={<Loading/>}>
+      <Header/>
       <Countrylist />
     </Suspense>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <nav class="navbar navbar-light bg-dark ">
+      <div class="container-fluid ">
+        <span class="navbar-brand text-light" >
+          <h1>Country List</h1>
+        </span>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
